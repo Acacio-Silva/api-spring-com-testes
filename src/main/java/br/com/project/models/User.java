cca41b4a63ca9edc144129be79029d6de7fbf18e
Acacio-Silva <acaciosilva90@gmail.com>
2022-11-07 11:08:05 -0300

    Adicionando Classe User

---
+++ src/main/java/br/com/project/models/User.java
@@ -0,0 +1,29 @@
+package br.com.project.models;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@EqualsAndHashCode.Include
+	private Integer id;
+	private String name;
+	@Column(unique = true)
+	private String email;
+	private String senha;
+	
+}
