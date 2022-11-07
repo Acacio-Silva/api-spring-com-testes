b69314039875b33947e665dc5563766a8e3a8e51
Acacio-Silva <acaciosilva90@gmail.com>
2022-11-07 15:39:17 -0300

    endpoit delete

--- src/main/java/br/com/project/resources/UserResource.java
+++ src/main/java/br/com/project/resources/UserResource.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,13 +25,15 @@ import br.com.project.services.UserService;
 @RequestMapping(value = "/user")
 public class UserResource {
 	
+	private static final String ID = "/{id}";
+
 	@Autowired
 	private UserService service;
 	
 	@Autowired
 	private ModelMapper mapper;
 	
-	@GetMapping(value = "/{id}")
+	@GetMapping(value = ID)
 	public ResponseEntity<UserDTO> findById(@PathVariable Integer id){
 		return ResponseEntity.ok().body(mapper.map(service.findById(id), UserDTO.class));
 	}
@@ -44,14 +47,20 @@ public class UserResource {
 	
 	@PostMapping
 	public ResponseEntity<UserDTO> create(@RequestBody UserDTO userDTO){
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(service.create(userDTO)).toUri();
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path(ID).buildAndExpand(service.create(userDTO)).toUri();
 		return ResponseEntity.created(uri).build();
 	}
 	
-	@PutMapping(value = "/{id}")
+	@PutMapping(value = ID)
 	public ResponseEntity<UserDTO> update(@PathVariable Integer id, @RequestBody UserDTO dto){
 		dto.setId(id);
 		return ResponseEntity.ok().body(mapper.map(service.update(dto), UserDTO.class));
+	}
+	
+	@DeleteMapping(value = ID)
+	public ResponseEntity<UserDTO> delete(@PathVariable Integer id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 
 }
