798e3c74753fb33127d5739866e05632e681990c
Acacio-Silva <acaciosilva90@gmail.com>
2022-11-07 11:54:41 -0300

    adicionando perfil de teste (local)

---
+++ src/main/java/br/com/project/config/LocalConfig.java
@@ -0,0 +1,31 @@
+package br.com.project.config;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import br.com.project.models.User;
+import br.com.project.repositories.UserRepository;
+
+@Configuration
+@Profile("local")
+public class LocalConfig{
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Bean
+	public void iniciaDB(){
+		
+		User user1 = new User(null, "Acacio", "acacio@email.com", "12345");
+		User user2 = new User(null, "Francisco", "francisco@email.com", "1234567");
+		
+		userRepository.saveAll(List.of(user1, user2));
+	}
+
+	
+}
