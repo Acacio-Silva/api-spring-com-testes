afb5c81f68e2260a66ca9b0a7892e510bc084f3e
Acacio-Silva <acaciosilva90@gmail.com>
2022-11-08 11:05:34 -0300

    teste update com excecao

--- src/test/java/br/com/project/services/impl/UserServiceImplTest.java
+++ src/test/java/br/com/project/services/impl/UserServiceImplTest.java
@@ -23,7 +23,7 @@ import br.com.project.services.exceptions.DataIntegratyViolationException;
 import br.com.project.services.exceptions.ObjectNotFoundException;
 
 class UserServiceImplTest {
-	
+
 	private static final String SENHA = "12345";
 
 	private static final String EMAIL = "acacio@gmail.com";
@@ -34,18 +34,17 @@ class UserServiceImplTest {
 
 	@InjectMocks
 	private UserServiceImpl serviceImpl;
-	
+
 	@Mock
 	private UserRepository repository;
 	@Mock
 	private ModelMapper mapper;
-	
+
 	private User user;
-	
+
 	private UserDTO userDTO;
-	
+
 	private Optional<User> optionalUser;
-	
 
 	@BeforeEach
 	void setUp() throws Exception {
@@ -57,37 +56,36 @@ class UserServiceImplTest {
 	void testFindById() {
 		when(repository.findById(Mockito.anyInt())).thenReturn(optionalUser);
 		User response = serviceImpl.findById(_ID);
-		
+
 		assertNotNull(response);
 		assertEquals(User.class, response.getClass());
 		assertEquals(_ID, response.getId());
 		assertEquals(NOME, response.getName());
 		assertEquals(EMAIL, response.getEmail());
 		assertEquals(SENHA, response.getSenha());
-		
+
 	}
-	
+
 	@Test
 	void findByIdObjectNotFoundException() {
-		
+
 		when(repository.findById(Mockito.anyInt())).thenThrow(new ObjectNotFoundException("usuario não encontrado!"));
-		
+
 		try {
 			serviceImpl.findById(_ID);
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			assertEquals(ObjectNotFoundException.class, e.getClass());
 			assertEquals("usuario não encontrado!", e.getMessage());
 		}
-		
+
 	}
 
 	@Test
 	void testFindAll() {
 		when(repository.findAll()).thenReturn(List.of(user));
-		
+
 		List<User> response = serviceImpl.findAll();
-		
+
 		assertNotNull(response);
 		assertEquals(1, response.size());
 		assertEquals(User.class, response.get(0).getClass());
@@ -96,28 +94,25 @@ class UserServiceImplTest {
 
 	@Test
 	void testCreate() {
-		
+
 		when(repository.save(Mockito.any())).thenReturn(user);
-		
+
 		User response = serviceImpl.create(userDTO);
-		
+
 		assertNotNull(response);
 		assertEquals(User.class, response.getClass());
 		assertEquals(_ID, response.getId());
 		assertEquals(NOME, response.getName());
 		assertEquals(EMAIL, response.getEmail());
 		assertEquals(SENHA, response.getSenha());
-		
-		
-		
-	}
 
+	}
 
 	@Test
 	void testCreateDataIntegratyViolationException() {
-		
+
 		when(repository.findByEmail(Mockito.anyString())).thenReturn(optionalUser);
-		
+
 		try {
 			optionalUser.get().setId(2);
 			serviceImpl.create(userDTO);
@@ -125,21 +120,45 @@ class UserServiceImplTest {
 			assertEquals(DataIntegratyViolationException.class, e.getClass());
 			assertEquals("Email já cadastrado!", e.getMessage());
 		}
-				
+
 	}
 
-	
+	@Test
+	void testUpdate() {
+
+		when(repository.save(Mockito.any())).thenReturn(user);
+
+		User response = serviceImpl.update(userDTO);
+
+		assertNotNull(response);
+		assertEquals(User.class, response.getClass());
+		assertEquals(_ID, response.getId());
+		assertEquals(NOME, response.getName());
+		assertEquals(EMAIL, response.getEmail());
+		assertEquals(SENHA, response.getSenha());
+
+	}
 	
 	
 	@Test
-	void testUpdate() {
+	void testUpdateDataIntegratyViolationException() {
+
+		when(repository.findByEmail(Mockito.anyString())).thenReturn(optionalUser);
+
+		try {
+			optionalUser.get().setId(2);
+			serviceImpl.create(userDTO);
+		} catch (Exception e) {
+			assertEquals(DataIntegratyViolationException.class, e.getClass());
+			assertEquals("Email já cadastrado!", e.getMessage());
+		}
+
 	}
 
 	@Test
 	void testDelete() {
 	}
-	
-	
+
 	private void startUser() {
 		user = new User(_ID, NOME, EMAIL, SENHA);
 		userDTO = new UserDTO(1, NOME, EMAIL, SENHA);
